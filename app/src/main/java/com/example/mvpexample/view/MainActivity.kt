f4e3a7a22ebaf3d94422022491c4197eb9eb6e10
Noah Doperalski <55458814+ndoperalski130@users.noreply.github.com>
2022-05-18 09:56:17 -0400

    Initial Commit

--- app/src/main/java/com/example/mvpexample/view/MainActivity.kt
+++ app/src/main/java/com/example/mvpexample/view/MainActivity.kt
@@ -2,8 +2,13 @@ package com.example.mvpexample.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.View.*
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mvpexample.R
+import com.example.mvpexample.databinding.ActivityMainBinding
+import com.example.mvpexample.databinding.MovieShmovieLayoutBinding
 import com.example.mvpexample.model.Movie
 import com.example.mvpexample.presenter.MoviePresenter
 
@@ -12,18 +17,31 @@ class MainActivity : AppCompatActivity() , MainView{
     private lateinit var moviePresenter: MoviePresenter
     private lateinit var recyclerView: RecyclerView
 
+    // viewBinding
+    // pattern to prevent memory leaks, LeakCanary
+    private var _binding : ActivityMainBinding? = null
+    private val binding : ActivityMainBinding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        
+        setContentView(binding.root)
+
+
+        
     }
 
     override fun showProgress() {
-
+        binding.pbLoading.visibility = View.VISIBLE
+        binding.rvMovies.visibility = GONE
+        binding.tvErrorText.visibility = GONE
         //TODO("Not yet implemented")
     }
 
     override fun hideProgress() {
-
+        binding.pbLoading.visibility = GONE
         //TODO("Not yet implemented")
     }
 
@@ -33,7 +51,17 @@ class MainActivity : AppCompatActivity() , MainView{
     }
 
     override fun setError(msg: String) {
+//        binding.tvErrorText.text = msg
+//        binding.tvErrorText.visibility = VISIBLE
+//        binding.root.visibility = INVISIBLE
+        binding.tvErrorText.apply {
+            text = msg
+            visibility = VISIBLE
+        }
+    }
 
+    override fun onDestroy() {
+        super.onDestroy()
     }
 }
 
