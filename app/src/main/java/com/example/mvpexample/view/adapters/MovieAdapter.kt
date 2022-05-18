ab38f91675995a669a7059961bf3973f7195defc
Noah Doperalski <55458814+ndoperalski130@users.noreply.github.com>
2022-05-18 10:13:27 -0400

    Wednesday Class Added Glide

--- app/src/main/java/com/example/mvpexample/view/adapters/MovieAdapter.kt
+++ app/src/main/java/com/example/mvpexample/view/adapters/MovieAdapter.kt
@@ -1,18 +1,50 @@
 package com.example.mvpexample.view.adapters
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.mvpexample.databinding.MovieListItemBinding
 import com.example.mvpexample.model.Movie
 
-class MovieAdapter(private val movies: MutableList<Movie> = mutableListOf()) {
+class MovieAdapter(private val movies: MutableList<Movie> = mutableListOf()) : RecyclerView.Adapter<MovieAdapter.MovieViewHolder>(){
 
     fun setMovieList(list: List<Movie>)
     {
         movies.addAll(list)
+
     }
 
     inner class MovieViewHolder(private val binding: MovieListItemBinding) : RecyclerView.ViewHolder(binding.root)
     {
+        fun onBind(movie: Movie)
+        {
+            binding.tvMovieTitle.text = movie.name
 
+            Glide.with(binding.ivPoster)
+                .load(movie.imageUrl)
+                .into(binding.ivPoster)
+
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+
+        return MovieViewHolder(
+            MovieListItemBinding.inflate(LayoutInflater.from(parent.context),
+                parent,
+                false)
+        )
+        //TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+        holder.onBind(movies[position])
+        //TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        return movies.size
+        //TODO("Not yet implemented")
     }
 }
