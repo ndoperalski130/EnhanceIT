90ceef3dcac47a21b4b0f99c0a81002ee8ccf722
Noah Doperalski <55458814+ndoperalski130@users.noreply.github.com>
2022-05-18 10:03:15 -0400

    Wednesday Class Added RecyclerView

--- app/src/main/java/com/example/mvpexample/view/adapters/MovieAdapter.kt
+++ app/src/main/java/com/example/mvpexample/view/adapters/MovieAdapter.kt
@@ -1,6 +1,18 @@
 package com.example.mvpexample.view.adapters
 
+import androidx.recyclerview.widget.RecyclerView
+import com.example.mvpexample.databinding.MovieListItemBinding
 import com.example.mvpexample.model.Movie
 
 class MovieAdapter(private val movies: MutableList<Movie> = mutableListOf()) {
+
+    fun setMovieList(list: List<Movie>)
+    {
+        movies.addAll(list)
+    }
+
+    inner class MovieViewHolder(private val binding: MovieListItemBinding) : RecyclerView.ViewHolder(binding.root)
+    {
+
+    }
 }
